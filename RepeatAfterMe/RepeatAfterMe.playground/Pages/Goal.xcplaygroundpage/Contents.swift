101c8438321176fb29629d874ff04dd19f612f56
Ben Laurin <benlaurin@Bens-MacBook-Pro-2.local>
2017-02-15 10:41:50 -0500

    Finished big circle and circle and square patterns. Starting to try and figure out the little random circles

--- RepeatAfterMe/RepeatAfterMe.playground/Pages/Goal.xcplaygroundpage/Contents.swift
+++ RepeatAfterMe/RepeatAfterMe.playground/Pages/Goal.xcplaygroundpage/Contents.swift
@@ -21,26 +21,46 @@ import PlaygroundSupport
 // Create a new canvas
 let canvas = Canvas(width: 360, height: 360)
 
-// Loop three times
-//for i in 1...5 {
-    
-//    // Generate random number between -75 and 75
-//    let offset = Int(arc4random_uniform(150)) - 75
-//    
-//    // Draw circles in random horizontal positions in the middle of the canvas
-//    canvas.drawEllipse(centreX: canvas.width / 2 + offset, centreY: 100 * i , width: 5, height: 5)
-
-//}
 
 // Modify code above as needed and continue writing code below as needed... 
 
-for plusone in stride(from: 0, through: 360, by: 1){
-    canvas.drawShapesWithBorders = true
-    canvas.drawShapesWithFill = false
-    canvas.borderColor = Color(hue: Float(plusone), saturation: 100, brightness: 100, alpha: 100)
-    canvas.drawEllipse(centreX: 180, centreY: 180, width: plusone, height: plusone)
-}
+//for plusone in stride(from: 0, through: 360, by: 2){
+//    canvas.drawShapesWithBorders = true
+//    canvas.drawShapesWithFill = false
+//    canvas.borderColor = Color(hue: Float(plusone), saturation: 100, brightness: 100, alpha: 100)
+//    canvas.drawEllipse(centreX: 180, centreY: 180, width: plusone, height: plusone)
+//}
 
+//for leftcirc in stride(from: 0, through: 100, by: 10){
+//    canvas.drawShapesWithBorders = true
+//    canvas.drawShapesWithFill = false
+//    canvas.borderColor = Color.black
+//    canvas.drawEllipse(centreX: 50, centreY: 310, width: leftcirc, height: leftcirc)
+//    
+//}
+//
+//for rightrec in stride(from: 10, through: 100, by: 10){
+//    canvas.drawShapesWithBorders = true
+//    canvas.drawShapesWithFill = false
+//    canvas.borderColor = Color.black
+//    canvas.drawRectangle(bottomLeftX: 250 + rightrec, bottomLeftY: 0, width: 110 - rightrec, height: 110 - rightrec, borderWidth: 1)
+//}
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 360, height: 360)
+
+// Loop three times
+for i in 1...10 {
+
+    // Generate random number between -75 and 75
+    let offset = Int(arc4random_uniform(150)) - 75
+
+    // Draw circles in random horizontal positions in the middle of the canvas
+    canvas.drawShapesWithFill = true
+    canvas.drawShapesWithBorders = false
+    canvas.fillColor = Color.white
+    canvas.drawEllipse(centreX: canvas.width / 2 + offset, centreY: 100 * i , width: 10, height: 10)
+    
+
+}
 //: ## Template code
 //: The code below is necessary to see the result of your work in the Assistant Editor at right. Please do not remove.
 PlaygroundPage.current.liveView = canvas.imageView
